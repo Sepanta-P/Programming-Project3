e46947a1f8ef590f4f4dadfb8f3ed66c8903ab7e
Sepanta-P <440015243@gapps.yrdsb.ca>
2025-01-16 19:33:29 -0500

    init c

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;  //Password is Admin123
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to our shopping Website. Please enter the shopping category you want to explore: ");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.println("Electronics.");
+        ArrayList <String> e =new ArrayList<String>();
+        e.add("Iphone 16 pro max");
+        e.add("Airpods Pro");
+        e.add("Laptop");
+        e.add("Tablet");
+        e.add("Tevision");
+
+
+
+
+        System.out.println("Groceries");
+        ArrayList <String> g=new ArrayList<String>();
+        g.add("Apples");
+        g.add("Oranges");
+        g.add("Cucumbers");
+        g.add("Mango");
+
+
+
+
+
+        System.out.println("Films");
+        ArrayList <String> f=new ArrayList<String>();
+        f.add("SpiderMan");
+        f.add("Batman");
+        f.add("Inside Out");
+
+
+        System.out.println("Books");
+        System.out.println("Admin");
+        String category = sc.nextLine();
+
+
+
+
+        ArrayList<String> Books=new ArrayList<String>();
+
+
+        int cost=0;
+        switch (category) {
+            case "Electronics":
+
+
+                break;
+            case "Groceris":
+
+
+                break;
+
+
+            case "Films":
+
+
+                break;
+
+
+            case "Books":
+
+
+                break;
+
+
+            case "Admin":
+
+
+                break;
         }
+
+
     }
 }
